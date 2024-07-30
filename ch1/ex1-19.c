4b218753e87af98e5a7fe8c885436156f859cb1d
Billy <wcremeika@gmail.com>
2024-07-30 05:49:16 -0400

    fix: ex1-19

--- ch1/ex1-19.c
+++ ch1/ex1-19.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
-void reverse(char s[]);
-int length(char s[]);
+int len(char *str) {
+  int i = 0;
+  while (str[i] != '\0') i++;
+  return i;
+}
+
+void reverse(char str[]) {
+    int left = 0;
+    int right = len(str) - 1;
+    char temp;
+
+    while (left < right) {
+        temp = str[left];
+        str[left++] = str[right];
+        str[right--] = temp;
+
+    }
+}
 
 int main() {
-  reverse("hello\0");
-}
-
-int length(char s[]) {
-  int l = 0;
-
-  while (s[l++] != '\0');
-
-  return l;
-}
-
-void reverse(char s[]) {
-  int len = length(s);
-  int i = len - 1;
-
-  while (i >= 0) {
-    putchar(s[i--]);
-  }
+    char str[] = "Hello, World!";
+    printf("Original string: %s\n", str);
+    reverse(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
 }
 
